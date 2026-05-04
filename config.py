910e4f78bdc36571bbd9815703e05d6b46f6ed60
Shanky Ram <shankyram@google.com>
2026-05-04 20:02:54 +0800

    v13.0.0

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ import os
 from tools import Tools
 
 # Model Configurations
-ORCHESTRATOR_MODEL = "gemini-live-2.5-flash-native-audio"
+#ORCHESTRATOR_MODEL = "gemini-live-2.5-flash-native-audio"
+ORCHESTRATOR_MODEL = "gemini-3.1-flash-live-preview"
 SUBAGENT_MODEL = "gemini-2.5-flash-lite"
 
 # App Configuration
