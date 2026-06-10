db28a678e719212075018fea60cbd30a5f8eb315
Shanky Ram <shankyram@google.com>
2026-06-10 16:53:12 +0800

    v18.1.8.2

--- main.py
+++ main.py
@@ -433,9 +433,9 @@ async def websocket_endpoint(
                     if(event_type in ("function_call", "function_response")):                
                         logger.info(f"### RESPONSE TO FRONTEND - {event_json}")
                         if(event_type in ("function_call")):
-                            usage_logger.info(f"function_call {len(json.dumps(event_json)}")
+                            usage_logger.info(f"function_call {len(json.dumps(event_json))}")
                         else:
-                            usage_logger.info(f"function_response {len(json.dumps(event_json)}")
+                            usage_logger.info(f"function_response {len(json.dumps(event_json))}")
                     await websocket.send_text(event_json)                    
             else:                
                 # logger.info(f"### RESPONSE TO FRONTEND - {event_json}")
